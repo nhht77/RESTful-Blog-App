5e74c05c19874e1d19fa020afa85b5f6c632cea5
nhht77 <35263094+nhht77@users.noreply.github.com>
2018-10-11 00:34:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# RESTful-Blog-App
+
+##Blog Index
+
+Setup the Blog App
+Create the Blog model
+Add INDEX route and template
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = 3000;
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// MODEL CONFIG
+const Schema = mongoose.Schema;
+ 
+const BlogSchema = new Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+
+const Blog =  mongoose.model('Blog', BlogSchema);
+
+// Blog.create({
+//     title: 'First post',
+//     image: 'https://images.unsplash.com/photo-1523057530100-383d7fbc77a1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cff1b2c9744c71e84145dd78928513fc&auto=format&fit=crop&w=1049&q=80',
+//     body:'This is the first blog post'
+// }, blogs => {
+    // console.log(blogs);
+// })
+
+// RESTful ROUTES
+app.get("/", (req, res) => {
+    res.redirect('/blogs');
+})
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            res.render('index.ejs', {blogs: blogs});
+        }
+    })
+    
+})
+
+app.listen(PORT, () => {
+    console.log('RESTful Blog App Server is running on port: ' + PORT);
+})
